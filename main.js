904a24bf9d114f4d9ae07822291acb74958b086e
Bruno Almeida <brunoalmeida2846@hotmail.com>
2024-01-11 20:13:35 -0300

    Atualizando o Javascript

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
 const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./img/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./img/reprovado.png" alt="Emoji decepcionado" />';
+const atividades = [];
+const notas = [];
+
+let linhas = '';
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
+    adicionaLinha();
+    atualizaTabela();
+    atualizaMediaFinal();
+});
+
+function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
-    alert(`Atividade: ${inputNomeAtividade.value} - Nota: ${inputNotaAtividade.value}`);
-});
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    inputNomeAtividade.value = ' ';
+    inputNotaAtividade.value = ' ';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function atualizaMediaFinal() {
+    console.log(atividades);
+    console.log(notas);
+}
